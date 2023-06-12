37230e95ac247543b0609fc6df545e557755d84e
beryl <beryl@Sentinel>
2023-06-12 22:46:27 +0530

    Work from Acer

--- src/test/java/com/tutorialsninja/qa/testcases/Login.java
+++ src/test/java/com/tutorialsninja/qa/testcases/Login.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -21,8 +22,8 @@ public class Login {
 		driver.get("https://tutorialsninja.com/demo/");
 		driver.findElement(By.xpath("//span[text()='My Account']")).click();
 		driver.findElement(By.linkText("Login")).click();
-		driver.findElement(By.id("input-email")).sendKeys("");
-		driver.findElement(By.id("input-password")).sendKeys("");
+		driver.findElement(By.id("input-email")).sendKeys(Keys.chord(Keys.CONTROL,"a"),"ABCD");
+		driver.findElement(By.id("input-password")).sendKeys(Keys.chord(Keys.CONTROL,"a"),"123");
 		
 		
 		
